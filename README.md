2d51dbd20d6c06aa152f595df2297f6f4c667a09
Robin Winslow <robin@robinwinslow.co.uk>
2021-10-05 18:31:43 +0800

    All the code

---
+++ README.md
@@ -0,0 +1,32 @@
+# Create meet using Google Calendar API
+
+This script creates a meet using the Google Calendar API by creating a calendar meeting with a conference call.
+
+## Installation
+
+``` bash
+# Install requirements
+python3 -m venv .env3
+source .env3/bin/activate
+pip3 install -r requirements.txt
+```
+
+## Getting secrets
+
+Go to the [mattermost meet oauth2 credential page](https://console.cloud.google.com/apis/credentials/oauthclient/399896422909-1c2oeats1d48gtri7usfque3u47635qe.apps.googleusercontent.com?authuser=1&project=mattermost-meet-328008) and click "Download JSON". Save the file as `user.json` alongside this project's `meet.py`.
+
+## Usage
+
+``` bash
+python3 meet.py
+```
+
+The first time you run this it will open a browser window. You should authorize with your organisation account.
+
+Once authorized correctly, it will generate a meet code. On subsequent runs it shouldn't require authorization.
+
+## Service accounts vs user accounts
+
+It could in theory be possible to use a service account instead of your user account. E.g. there's a [service account credential here](https://console.cloud.google.com/iam-admin/serviceaccounts/details/105695916903695317913;edit=true?previousPage=%2Fapis%2Fcredentials%3Fauthuser%3D1%26project%3Dmattermost-meet-328008&authuser=1&project=mattermost-meet-328008). You would simply change the code to use the `get_service_account_credentials` function instead of the `get_user_account_credentials` one.
+
+The trouble is that a bare service account [can't create conference calls](https://stackoverflow.com/questions/61050432/cant-create-a-hangoutsmeet-with-calendar-api-using-java). Instead it needs to impersonate a user. To do that, it needs [domain-wide delegation of authority](https://developers.google.com/cloud-search/docs/guides/delegation) which must be granted by an organisation administrator.
---
+++ meet.py
@@ -0,0 +1,72 @@
+import time
+import os.path
+from google_auth_oauthlib.flow import InstalledAppFlow
+from google.auth.transport.requests import Request
+from google.oauth2.credentials import Credentials
+from google.oauth2 import service_account
+import googleapiclient.discovery
+
+
+SCOPES = ["https://www.googleapis.com/auth/calendar"]
+
+
+def get_service_account_credentials():
+    return service_account.Credentials.from_service_account_file(
+        "service.json", scopes=SCOPES
+    )
+
+
+def get_user_account_credentials():
+    credentials = None
+    # The file token.json stores the user's access and refresh tokens, and is
+    # created automatically when the authorization flow completes for the first
+    # time.
+    if os.path.exists("token.json"):
+        credentials = Credentials.from_authorized_user_file(
+            "token.json", SCOPES
+        )
+    # If there are no (valid) credentials available, let the user log in.
+    if not credentials or not credentials.valid:
+        if credentials and credentials.expired and credentials.refresh_token:
+            credentials.refresh(Request())
+        else:
+            flow = InstalledAppFlow.from_client_secrets_file(
+                "user.json", SCOPES
+            )
+            credentials = flow.run_local_server(port=0)
+        # Save the credentials for the next run
+        with open("token.json", "w") as token:
+            token.write(credentials.to_json())
+
+    return credentials
+
+
+event_body = {
+    "summary": "Sample event",
+    "description": "This is a sample event.",
+    "start": {
+        "dateTime": "2015-05-28T09:00:00-07:00",
+        "timeZone": "America/Los_Angeles",
+    },
+    "end": {
+        "dateTime": "2015-05-28T17:00:00-07:00",
+        "timeZone": "America/Los_Angeles",
+    },
+    "conferenceData": {
+        "createRequest": {
+            "requestId": str(time.time())[:10],
+            "conferenceSolutionKey": {"type": "hangoutsMeet"},
+        }
+    },
+}
+
+calendar = googleapiclient.discovery.build(
+    "calendar", "v3", credentials=get_user_account_credentials()
+)
+event = (
+    calendar.events()
+    .insert(calendarId="primary", conferenceDataVersion=1, body=event_body)
+    .execute()
+)
+
+print(f'Meet created: {event.get("hangoutLink")}')
